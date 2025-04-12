5cf28f2f43ddf328ef30b967598160db27701158
Marian-30 <chowdhurymarian30@gmail.com>
2025-04-12 22:44:16 +0600

    Add files via upload

---
+++ FunctionVowelConsonant.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void checkVowelOrConsonant(char ch) {
+    if ((ch == 'a') || (ch == 'e') || (ch == 'i') || (ch == 'o') || (ch == 'u') ||
+        (ch == 'A') || (ch == 'E') || (ch == 'I') || (ch == 'O') || (ch == 'U')) {
+        printf("'%c' is a vowel.\n", ch);
+    }
+    else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+        printf("'%c' is a consonant.\n", ch);
+    }
+    else {
+        printf("'%c' is not an alphabet character.\n", ch);
+    }
+}
+int main() {
+    char ch;
+
+    printf("Enter a character: ");
+    scanf(" %c", &ch);
+
+    checkVowelOrConsonant(ch);
+
+    return 0;
+}
+
