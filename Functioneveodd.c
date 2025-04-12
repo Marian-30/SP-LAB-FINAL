04a06d23122174ff83da925780cf69e7a80e62c6
Marian-30 <chowdhurymarian30@gmail.com>
2025-04-12 21:58:08 +0600

    Add files via upload

--- Functioneveodd.c
+++ Functioneveodd.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+
+void checkEvenOdd(int num) {
+    if (num % 2 == 0) {
+        printf("%d is even.\n", num);
+    } else {
+        printf("%d is odd.\n", num);
+    }
+}
+
+int main() {
+    int num;
+
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    checkEvenOdd(num);
+
+    return 0;
+}
+
+
 
